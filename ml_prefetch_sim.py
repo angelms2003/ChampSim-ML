fae19a3a11539af0a79e4157d7369e90bb99aea7
Ángel <angel.martinezs@um.es>
2026-05-12 12:05:22 +0200

    n_trials fix

--- ml_prefetch_sim.py
+++ ml_prefetch_sim.py
@@ -548,9 +548,10 @@ def optuna_train_and_test_command():
     # The Optuna optimizer is created
     optimizer = OptunaHyperparameterSearch(train_data, test_data, args.experiments_dir)
 
-    # The optimization is executed and the best parameters are found
+    # The optimization is executed and the best parameters are
+    # found. The optimization is run for 50 trials
     study_name = args.model_name.split("/")[-1]
-    best_params = optimizer.optimize(study_name, n_trials=2)
+    best_params = optimizer.optimize(study_name, n_trials=50)
 
     # If there is no lookahead size, it means that we set it to a
     # fixed value of 0 when training
